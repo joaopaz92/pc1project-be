425f9418da7f65a2b9cd41b364cfd7dc4408519d
Joao P. Azevedo <joaopedro.azevedosn@gmail.com>
2023-06-30 01:04:58 +0100

    order setup

---
+++ database/migrations/2023_06_18_214138_create_lessons_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('lessons', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome');
+            $table->integer('idade');
+            $table->string('trabalho');
+            $table->boolean('reformado')->default(false);
+            $table->string('email');
+            $table->boolean('aceita')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('lessons');
+    }
+};
